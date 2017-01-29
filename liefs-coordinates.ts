d7504a4207ab9c749a37a1196f5cd315ecac2401
Leafdriving <leafdriving@hotmail.com>
2017-01-28 20:26:02 -0500

    First Commit

---
+++ liefs-coordinates.ts
@@ -0,0 +1,8 @@
+export class Coord {
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+  constructor (width: number = 0, height: number = 0, x: number = 0, y: number = 0) {
+    this.width = width; this.height = height; this.x = x; this.y = y; }
+}
