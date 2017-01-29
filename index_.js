60353720c9da058c7d8be722858db5c8e5a73817
Leafdriving <leafdriving@hotmail.com>
2017-01-29 00:39:02 -0500

    add _

---
+++ index_.js
@@ -0,0 +1,13 @@
+var Coord = (function () {
+    function Coord(width, height, x, y) {
+        if (width === void 0) { width = 0; }
+        if (height === void 0) { height = 0; }
+        if (x === void 0) { x = 0; }
+        if (y === void 0) { y = 0; }
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+    }
+    return Coord;
+}());
