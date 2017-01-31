7cc963be7b822e33cf36761034785c0a83e27eb7
Leafdriving <leafdriving@hotmail.com>
2017-01-31 02:01:52 -0500

    somethings fishy

--- index.ts
+++ index.ts
@@ -1,4 +1,18 @@
 export class Coord {
+    static byEl(el, byRoot: boolean = true) {
+        if (!el) return Coord.byWin();
+        let x: number = el.offsetLeft, y: number = el.offsetTop;
+        if (byRoot) for (x = 0, y = 0; el != null;
+            x += el.offsetLeft, y += el.offsetTop, el = el.offsetParent);
+        return new Coord(el.style.width, el.style.height, x, y);
+    }
+    static byWin() {
+        let w = window, d = document, e = d.documentElement,
+            g = d.getElementsByTagName("body")[0],
+            width = w.innerWidth || e.clientWidth || g.clientWidth,
+            height = w.innerHeight || e.clientHeight || g.clientHeight;
+        return new Coord(width, height, 0, 0);
+    }
     width: number;
     height: number;
     x: number;
--- index_.js
+++ index_.js
@@ -9,5 +9,19 @@ var Coord = (function () {
         this.x = x;
         this.y = y;
     }
+    Coord.byEl = function (el, byRoot) {
+        if (byRoot === void 0) { byRoot = true; }
+        if (!el)
+            return Coord.byWin();
+        var x = el.offsetLeft, y = el.offsetTop;
+        if (byRoot)
+            for (x = 0, y = 0; el != null; x += el.offsetLeft, y += el.offsetTop, el = el.offsetParent)
+                ;
+        return new Coord(el.style.width, el.style.height, x, y);
+    };
+    Coord.byWin = function () {
+        var w = window, d = document, e = d.documentElement, g = d.getElementsByTagName("body")[0], width = w.innerWidth || e.clientWidth || g.clientWidth, height = w.innerHeight || e.clientHeight || g.clientHeight;
+        return new Coord(width, height, 0, 0);
+    };
     return Coord;
 }());
